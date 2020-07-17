1d80abff2023ea04d2f3ef1a34b46c65b5efc1b4
Duy Nguyen <nguyenduy010798@gmail.com>
2020-07-17 13:55:54 +0300

    Updates

---
+++ precache-manifest.ed17359a77e753de7a16dbb864646834.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f39818dd2b081807dd26d14fa374f783",
+    "url": "/Fortnite-Store/index.html"
+  },
+  {
+    "revision": "c8f96264abca212edbd5",
+    "url": "/Fortnite-Store/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a870f1e141c4508aaaa4",
+    "url": "/Fortnite-Store/static/css/main.968d6415.chunk.css"
+  },
+  {
+    "revision": "c8f96264abca212edbd5",
+    "url": "/Fortnite-Store/static/js/2.ab6a5a5c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Fortnite-Store/static/js/2.ab6a5a5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a870f1e141c4508aaaa4",
+    "url": "/Fortnite-Store/static/js/main.555faa0f.chunk.js"
+  },
+  {
+    "revision": "dabefffda19b51b21c99",
+    "url": "/Fortnite-Store/static/js/runtime-main.8b7769f4.js"
+  },
+  {
+    "revision": "70849d8ea8dd7be00dc599c4bf229e0f",
+    "url": "/Fortnite-Store/static/media/daily-item.70849d8e.jpg"
+  },
+  {
+    "revision": "1683a22e3fa4de05fcf2df7183d7dd18",
+    "url": "/Fortnite-Store/static/media/first.1683a22e.jpg"
+  },
+  {
+    "revision": "86ab0c7796b3c57cbb43ed928ea1ea85",
+    "url": "/Fortnite-Store/static/media/fourth.86ab0c77.png"
+  },
+  {
+    "revision": "05d059daa0ee9fa21cf3957335f08f74",
+    "url": "/Fortnite-Store/static/media/second.05d059da.png"
+  },
+  {
+    "revision": "32185481919df82280857285a141afd6",
+    "url": "/Fortnite-Store/static/media/third.32185481.png"
+  }
+]);
